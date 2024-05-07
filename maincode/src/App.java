f0fb880ed442e23c6af9ea99828bc2420a27ff93
iceischilly <matteo.dare-robertson@student.tdsb.on.ca>
2024-05-07 13:56:14 -0400

    a

---
+++ maincode/src/App.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        trivia triviaObject = new trivia();
+        triviaObject.Guess();
+
+
+
+    }
+}
---
+++ maincode/src/trivia.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class trivia{
+    int = "points";
+
+    public void Guess(){
+        String title = "TRIVIA GAME";
+        String questions1 = "does matteo have autism";
+        String Guess =
+    }
+
+
+
+}
+
+
