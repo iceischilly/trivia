4af71d038c2fe9bb6813dead80b074131fdd437a
Ati14 <ati.gobin@student.tdsb.on.ca>
2024-05-13 13:55:15 -0400

    james

--- maincode/src/App.java
+++ maincode/src/App.java
@@ -1,9 +1,15 @@
 import javax.swing.JOptionPane;
 
+
 public class App {
+    
     public static void main(String[] args) throws Exception {
+        JOptionPane.showMessageDialog(null, "Welcome to the trivia");
         trivia triviaObject = new trivia();
         triviaObject.Guess();
+        
+
+
 
 
 
